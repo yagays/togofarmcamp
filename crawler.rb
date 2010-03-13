920d550a57531a52017b10f1f2eacfb3fb055f04
yag_ays <yanagi.ayase@gmail.com>
2010-03-14 00:38:51 +0900

    separation of action part and def part

--- crawler.rb
+++ crawler.rb
@@ -5,44 +5,40 @@ require 'open-uri'
 require 'kconv'
 require 'csv'
 
+def crawler(doc)
+  #Hash initialization
+  dateArray = Array.new
+  dbHash = Hash.new
 
-doc = Hpricot(open("/Users/yag_ays/togofarmcamp/togotv.html"))
+  #date
+  (doc/'h2 span.date').each_with_index {|elem,i| 
+    date =  elem.inner_text.gsub(/\s+/, "")
+    dateArray[i] = date
+  }
 
+  #title
+  (doc/'h3').each_with_index {|elem,i| 
+    title = elem.inner_text.toutf8.strip#delete("\n").delete("\t")#.gsub(/\s+/, "")   #行頭のスペースが取れない
+    if title[0,1] == "_"
+      dbHash[dateArray[i-3]] = title.gsub(/\[.*?\]/, "").delete("_")
+    end
+  }
 
-#matrix
-#e.g. [[date, title],[date, title]...]
-def nmarray(n,m)
-  (0...n).map{Array.new(m)}
+  return dbHash
 end
-db = nmarray(15,3)
 
-dateArray = Array.new
-dbHash = Hash.new
+def checkHash(hash)
+  hash.each{|key, value|
+    puts "#{key}:#{value}"
+  }
+end
 
-
-#date
-(doc/'h2 span.date').each_with_index {|elem,i| 
- date =  elem.inner_text.gsub(/\s+/, "")
- dateArray[i] = date
-}
-
-
-#title
-(doc/'h3').each_with_index {|elem,i| 
- title = elem.inner_text.toutf8.strip#delete("\n").delete("\t")#.gsub(/\s+/, "")   #行頭のスペースが取れない
-  if title[0,1] == "_"
-   dbHash[dateArray[i-3]] = title.gsub(/\[.*?\]/, "").delete("_")
-  end
-}
-
-#Hash確認用
-dbHash.each{|key, value|
-  puts "#{key}:#{value}"
-}
-
-#CSV output
-# CSV.generate("output.csv", ?,){|writer|
-#   db.each{|elem|
-#    writer << elem
-#   }
-# }
+def csvoutput(list)
+  output = list.to_a
+  puts output
+  CSV.generate("output.csv", ?,){|writer|
+    output.each{|elem|
+      writer << elem
+    }
+  }
+end
