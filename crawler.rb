ebd7b5d8cdbdfe0a01fa52ff198e4160eca19e3e
yag_ays <yanagi.ayase@gmail.com>
2010-03-13 20:27:41 +0900

    add crawler.rb

---
+++ crawler.rb
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+require 'rubygems'
+require 'hpricot'
+require 'open-uri'
+require 'kconv'
+
+doc = Hpricot(open("/Users/yag_ays/togofarmcamp/togotv.html"))
+
+
+#多次元配列作成
+def nmarray(n,m)
+  (0...n).map{Array.new(m)}
+end
+db = nmarray(15,2)
+
+
+#date
+(doc/'h2 span.date').each_with_index {|elem,i| 
+ date =  elem.inner_text.gsub(/\s+/, "")
+ db[i][0] = date
+}
+
+
+
+(doc/'h3').each_with_index {|elem,i| 
+ title = elem.inner_text.toutf8.strip#delete("\n").delete("\t")#.gsub(/\s+/, "")   #行頭のスペースが取れない
+  if title[0,1] == "_"
+   db[i-3][1] = title.gsub(/\[.*?\]/, "").delete("_")
+  end
+}
+puts db
+p db
+
+
+
+#  puts /\[.*?\]/ =~ hoge
